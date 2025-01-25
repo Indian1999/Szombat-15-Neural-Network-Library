b06e7fd0ff55dffb85d8194aa0c00a0cee5a8436
Indian1999 <haszab99@gmail.com>
2025-01-25 15:54:18 +0100

    activations

--- SmartGrass/activations.py
+++ SmartGrass/activations.py
@@ -11,3 +11,12 @@ def sigmoid(x):
 
 def sigmoid_deriv(x):
     return x * (1 - x)
+
+def relu(x):
+    return max([0, x])
+
+def relu_deriv(x):
+    if x > 0:
+        return 1
+    else:
+        return 0
