64e83c206157d14f620c2b674d883ab55898d0bb
Indian1999 <haszab99@gmail.com>
2025-02-01 16:14:10 +0100

    network fit

--- SmartGrass/network.py
+++ SmartGrass/network.py
@@ -14,8 +14,22 @@ class Network:
         self.loss = loss
         self.loss_deriv = loss_deriv
 
-    def fit():
-        pass
+    def fit(self, train_x, train_y, epochs, learning_rate):
+        samples = len(train_x)
+        for i in range(epochs):
+            error_in_epoch = 0
+            for j in range(samples):
+                output = train_x[j]
+                for layer in self.layers:
+                    output = layer.forward_propagation(output)
+                
+                error_in_epoch += self.loss(train_y[j], output)
+                error = self.loss_deriv(train_y[j], output)
+                for layer in reversed(self.layers):
+                    error = layer.backward_propagation(error, learning_rate)
+            error_in_epoch /= samples
+            print(f"Epoch No. {i+1}/{epochs}, loss = {error_in_epoch}")
+
 
     def predict(self, input):
         samples = len(input)
