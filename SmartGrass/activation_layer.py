03c2bafcb0783f5f510e0a934d66d52bc2246b1a
Indian1999 <haszab99@gmail.com>
2025-02-01 16:06:15 +0100

    activationLayer backward

--- SmartGrass/activation_layer.py
+++ SmartGrass/activation_layer.py
@@ -9,3 +9,6 @@ class Activation(Layer):
         self.input = input
         self.output = self.activation(self.input)
         return self.output
+    
+    def backward_propagation(self, output_error, learning_rate):
+        return output_error * self.activation_deriv(self.input)
